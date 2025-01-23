886cac2a73d86199f111dcb52019637c2dbb910a
47 PC1 <sinkvadev@gmail.com>
2025-01-23 10:32:27 +0700

    added char list loading + prettier config

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,52 +1,65 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import MarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
+import { useEffect, useState } from 'react';
 
 const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [charList, setCharList] = useState([]);
+
+	const marvelService = new MarvelService();
+
+	useEffect(() => {
+		marvelService.getAllCharacters().then(onCharListLoaded).catch(onError);
+	}, []);
+
+	const onCharListLoaded = (charList) => {
+		setCharList(charList);
+		setLoading(false);
+	};
+
+	const onError = () => {
+		setError(true);
+		setLoading(false);
+	};
+
+	const renderCharList = (arr) => {
+		const items = arr.map((item) => {
+			let imgStyle = { objectFit: 'cover' };
+			if (item.thumbnail.includes('image_not_available')) {
+				imgStyle = { objectFit: 'unset' };
+			}
+
+			return (
+				<li className="char__item" key={item.id}>
+					<img src={item.thumbnail} alt={item.name} style={imgStyle} />
+					<div className="char__name">{item.name}</div>
+				</li>
+			);
+		});
+		return items;
+	};
+
+	const items = renderCharList(charList);
+
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !(loading || error) ? items : null;
+
+	return (
+		<div className="char__list">
+			<ul className="char__grid" style={{ gridTemplateColumns: spinner ? '1fr' : 'repeat(3, 200px)' }}>
+				{errorMessage}
+				{spinner}
+				{content}
+			</ul>
+			<button className="button button__main button__long">
+				<div className="inner">load more</div>
+			</button>
+		</div>
+	);
+};
 
 export default CharList;
--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -6,88 +6,84 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 const RandomChar = () => {
+	const marvelService = new MarvelService();
 
-    const marvelService = new MarvelService();
+	const [char, setChar] = useState('');
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
 
-    const [char, setChar] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+	useEffect(() => {
+		updateChar();
+	}, []);
 
-    useEffect(() => {
-        updateChar();
-    }, []) 
+	const onCharLoaded = (char) => {
+		setError(false);
+		setChar(char);
+		setLoading(false);
+	};
 
-    const onCharLoaded = (char) => {
-        setError(false);
-        setChar(char);
-        setLoading(false);
-    }
+	const onError = () => {
+		setLoading(false);
+		setError(true);
+	};
 
-    const onError = () => {
-        setLoading(false);
-        setError(true);
-    }
-    
-    const updateChar = () => {
-        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        marvelService
-            .getCharacter(id)
-            .then(onCharLoaded)
-            .catch(onError)
-    }
+	const updateChar = () => {
+		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+		marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+	};
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? <View char={char} /> : null;
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !(loading || error) ? <View char={char} /> : null;
 
-    return (
-        <div className="randomchar">
-            {errorMessage}
-            {spinner}
-            {content}
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main" onClick={updateChar}>
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="randomchar">
+			{errorMessage}
+			{spinner}
+			{content}
+			<div className="randomchar__static">
+				<p className="randomchar__title">
+					Random character for today!
+					<br />
+					Do you want to get to know him better?
+				</p>
+				<p className="randomchar__title">Or choose another one</p>
+				<button className="button button__main" onClick={updateChar}>
+					<div className="inner">try it</div>
+				</button>
+				<img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+			</div>
+		</div>
+	);
+};
 
 const View = ({ char }) => {
-    
-    const { name, thumbnail, description, home, wiki } = char;
+	const { name, thumbnail, description, home, wiki } = char;
 
-    let thumbnailObjectFit = thumbnail.includes('image_not_available');
-    console.log(thumbnailObjectFit);
-    
-    return (
-        <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" style={thumbnailObjectFit ? {objectFit: 'contain'}: {objectFit: 'cover'}} />
-        <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">
-                {description}
-            </p>
-            <div className="randomchar__btns">
-                <a target='_blank' rel='noreferrer' href={home} className="button button__main">
-                    <div className="inner">homepage</div>
-                </a>
-                <a target='_blank' rel='noreferrer' href={wiki} className="button button__secondary">
-                    <div className="inner">Wiki</div>
-                </a>
-            </div>
-        </div>
-    </div>
-    )
-}
+	let thumbnailObjectFit = thumbnail.includes('image_not_available');
+
+	return (
+		<div className="randomchar__block">
+			<img
+				src={thumbnail}
+				alt="Random character"
+				className="randomchar__img"
+				style={thumbnailObjectFit ? { objectFit: 'contain' } : { objectFit: 'cover' }}
+			/>
+			<div className="randomchar__info">
+				<p className="randomchar__name">{name}</p>
+				<p className="randomchar__descr">{description}</p>
+				<div className="randomchar__btns">
+					<a target="_blank" rel="noreferrer" href={home} className="button button__main">
+						<div className="inner">homepage</div>
+					</a>
+					<a target="_blank" rel="noreferrer" href={wiki} className="button button__secondary">
+						<div className="inner">Wiki</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default RandomChar;
