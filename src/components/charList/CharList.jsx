a1c07782dc794246d88992e166762b81cf7f475f
47 PC1 <sinkvadev@gmail.com>
2025-01-26 03:39:03 +0700

    prop-type added

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -88,6 +89,10 @@ const CharList = ({ onCharSelected }) => {
 			</button>
 		</div>
 	);
+};
+
+CharList.propTypes = {
+	onCharSelected: PropTypes.func.isRequired,
 };
 
 export default CharList;
