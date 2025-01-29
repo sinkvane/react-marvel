b7dfb2340bec466745988b6e6de9794988a154d3
47 PC2 <sinkvadev@gmail.com>
2025-01-29 21:31:21 +0700

    charList fixed

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -10,7 +10,7 @@ const CharList = ({ onCharSelected }) => {
 	const [loading, setLoading] = useState(true);
 	const [charList, setCharList] = useState([]);
 	const [newItemLoading, setNewItemLoading] = useState(false);
-	const [offset, setOffset] = useState(1600);
+	const [offset, setOffset] = useState(0);
 	const [charEnded, setCharEnded] = useState(false);
 
 	const marvelService = new MarvelService();
