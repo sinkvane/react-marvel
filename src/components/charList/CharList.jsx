8d96036c2500d39cd6ad790b3ff3a1bedce94d67
47 PC2 <sinkvadev@gmail.com>
2025-01-30 03:11:15 +0700

    char focus added

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import PropTypes from 'prop-types';
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const CharList = ({ onCharSelected }) => {
 	const [error, setError] = useState(false);
 	const [loading, setLoading] = useState(true);
 	const [charList, setCharList] = useState([]);
 	const [newItemLoading, setNewItemLoading] = useState(false);
-	const [offset, setOffset] = useState(0);
+	const [offset, setOffset] = useState(210);
 	const [charEnded, setCharEnded] = useState(false);
 
 	const marvelService = new MarvelService();
@@ -38,7 +39,7 @@ const CharList = ({ onCharSelected }) => {
 		setCharList((prevCharList) => [...prevCharList, ...newCharList]);
 		setLoading(false);
 		setNewItemLoading(false);
-		setOffset(offset + 9);
+		setOffset((offset) => offset + 9);
 		setCharEnded(ended);
 	};
 
@@ -47,15 +48,38 @@ const CharList = ({ onCharSelected }) => {
 		setLoading(false);
 	};
 
+	const itemRefs = useRef([]);
+
+	const focusOnItem = (id) => {
+		itemRefs.current.forEach((item) => item.classList.remove('char__item_selected'));
+		itemRefs.current[id].classList.add('char__item_selected');
+		itemRefs.current[id].focus();
+	};
+
 	const renderCharList = (arr) => {
-		const items = arr.map((item) => {
+		const items = arr.map((item, i) => {
 			let imgStyle = { objectFit: 'cover' };
 			if (item.thumbnail.includes('image_not_available')) {
 				imgStyle = { objectFit: 'unset' };
 			}
 
 			return (
-				<li className="char__item" key={item.id} onClick={() => onCharSelected(item.id)}>
+				<li
+					className="char__item"
+					ref={(el) => (itemRefs.current[i] = el)}
+					tabIndex={0}
+					key={item.id}
+					onClick={() => {
+						onCharSelected(item.id);
+						focusOnItem(i);
+					}}
+					onKeyDown={(e) => {
+						if (e.key === '' || e.key === 'Enter') {
+							onCharSelected(item.id);
+							focusOnItem(i);
+						}
+					}}
+				>
 					<img src={item.thumbnail} alt={item.name} style={imgStyle} />
 					<div className="char__name">{item.name}</div>
 				</li>
@@ -72,7 +96,7 @@ const CharList = ({ onCharSelected }) => {
 
 	return (
 		<div className="char__list">
-			<ul className="char__grid" style={{ gridTemplateColumns: spinner ? '1fr' : 'repeat(3, 200px)' }}>
+			<ul className="char__grid" style={{ gridTemplateColumns: spinner ? '1fr' : 'repeat(3, 200px' }}>
 				{errorMessage}
 				{spinner}
 				{content}
